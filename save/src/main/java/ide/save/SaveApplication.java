88b448e063e30dfff7917db78895a702f1d9bb2e
woo <ldw28517@naver.com>
2024-01-16 22:42:36 +0900

    java 17 -> 11 & add submodule

--- save/src/main/java/ide/save/SaveApplication.java
+++ save/src/main/java/ide/save/SaveApplication.java
@@ -3,7 +3,9 @@ package ide.save;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+                org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+        })
 public class SaveApplication {
 
 	public static void main(String[] args) {
